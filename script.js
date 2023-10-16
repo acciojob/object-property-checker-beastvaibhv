32754c54c1f8cc08b4524206fdc9d4ccc15e5ddb
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-16 12:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  if(sampleObject.key){
+  if(sampleObject.[key]){
 	  return true;
   }
 	else{
